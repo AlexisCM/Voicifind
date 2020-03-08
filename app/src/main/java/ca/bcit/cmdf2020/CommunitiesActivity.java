f7f28b0f9849f35ccba78cd5444f271a8446cdc7
Alexis C. Mendiola <alexis_cm@live.com>
2020-03-08 11:56:59 -0700

    Made it pretty.

--- app/src/main/java/ca/bcit/cmdf2020/CommunitiesActivity.java
+++ app/src/main/java/ca/bcit/cmdf2020/CommunitiesActivity.java
@@ -2,14 +2,12 @@ package ca.bcit.cmdf2020;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.MenuItem;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
-
 import com.android.volley.Request;
 import com.android.volley.RequestQueue;
 import com.android.volley.Response;
@@ -17,18 +15,12 @@ import com.android.volley.VolleyError;
 import com.android.volley.toolbox.JsonObjectRequest;
 import com.android.volley.toolbox.Volley;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
-import com.google.firebase.database.DataSnapshot;
-import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
-import com.google.firebase.database.ValueEventListener;
-
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
-
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
 
 public class CommunitiesActivity extends AppCompatActivity {
