a1c4a428dabf20f0bc1377860241b254c1aaa201
glowiingo <gngo2@my.bcit.ca>
2020-03-08 05:56:14 -0700

    fix nav, update community and add post

--- app/src/main/java/ca/bcit/cmdf2020/CommunitiesActivity.java
+++ app/src/main/java/ca/bcit/cmdf2020/CommunitiesActivity.java
@@ -7,15 +7,47 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.MenuItem;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
 
 public class CommunitiesActivity extends AppCompatActivity {
+    private JSONObject jsonDBObject;
+    private String url = "https://voicifind.firebaseio.com/communities/.json";
+    int listLength;
+
+    DatabaseReference communitydbRef;
+    ListView communityListView;
+    List<String> communityList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_communities);
+
+        communitydbRef = FirebaseDatabase.getInstance().getReference("communities");
+        communityListView = findViewById(R.id.list_communities);
+        communityList = new ArrayList<>();
 
         BottomNavigationView navView = findViewById(R.id.nav_view_communities);
         navView.getMenu().getItem(0).setIcon(R.drawable.ic_home_black_24dp);
@@ -27,6 +59,8 @@ public class CommunitiesActivity extends AppCompatActivity {
             navView.getMenu().getItem(i).setChecked(false);
         }
         navView.getMenu().getItem(1).setChecked(true);
+
+        sendAndRequestResponse();
 
         navView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
             @Override
@@ -51,5 +85,49 @@ public class CommunitiesActivity extends AppCompatActivity {
                 return false;
             }
         });
+
+    }
+
+
+    private void sendAndRequestResponse() {
+        //RequestQueue initialized
+        RequestQueue mRequestQueue = Volley.newRequestQueue(this);
+
+        //String Request initialized
+        JsonObjectRequest mJsonRequest = new JsonObjectRequest(Request.Method.GET, url, null, new Response.Listener<JSONObject>() {
+            @Override
+            public void onResponse(JSONObject response) {
+                jsonDBObject = response;
+                Log.e("Test", response.toString());
+                Log.e("Test2", jsonDBObject.toString());
+                populatePage();
+            }
+
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Log.i("TAG", "Error :" + error.toString());
+            }
+        });
+        mRequestQueue.add(mJsonRequest);
+    }
+
+    public void populatePage() {
+        try {
+            JSONArray hashJsonArray = jsonDBObject.names();
+            assert hashJsonArray != null;
+            listLength = hashJsonArray.length();
+            List<String> ListOfCommunity = new ArrayList<>(listLength);
+            for (int i = 0; i < hashJsonArray.length(); i++) {
+                String key = hashJsonArray.get(i).toString();
+                JSONObject communityObj = (JSONObject) jsonDBObject.get(key);
+                String communityName = communityObj.getString("communityName");
+                ListOfCommunity.add(communityName);
+            }
+            ListView commListView = findViewById(R.id.list_communities);
+            commListView.setAdapter(new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, ListOfCommunity));
+        } catch (JSONException e) {
+            Log.e("userListConversion: ", e.toString());
+        }
     }
 }
--- app/src/main/java/ca/bcit/cmdf2020/PostsActivity.java
+++ app/src/main/java/ca/bcit/cmdf2020/PostsActivity.java
@@ -6,15 +6,35 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Spinner;
+import android.widget.TextView;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class PostsActivity extends AppCompatActivity {
+    DatabaseReference messagedbRef;
+    EditText makeNewPost;
+    Spinner communitySelect;
+    Button postButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_posts);
+
+        messagedbRef = FirebaseDatabase.getInstance().getReference("messages");
+        makeNewPost = (EditText) findViewById(R.id.make_new_post);
+        communitySelect = (Spinner) findViewById(R.id.community_spinner);
+        postButton = (Button) findViewById(R.id.postButton);
 
         BottomNavigationView navView = findViewById(R.id.nav_view_posts);
         navView.getMenu().getItem(0).setIcon(R.drawable.ic_home_black_24dp);
@@ -50,5 +70,40 @@ public class PostsActivity extends AppCompatActivity {
                 return false;
             }
         });
+
+        postButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (!makeNewPost.getText().toString().equals("")) {
+                    add_post();
+                    startActivity(new Intent(PostsActivity.this, HomepageActivity.class));
+                }
+            }
+        });
     }
+
+    private void add_post() {
+        String id = messagedbRef.push().getKey();
+        Message msg = new Message(id, communitySelect.getSelectedItem().toString(), makeNewPost.getText().toString());
+
+        Task setValueTask = messagedbRef.child(id).setValue(msg);
+
+        setValueTask.addOnSuccessListener(new OnSuccessListener() {
+            @Override
+            public void onSuccess(Object o) {
+                Toast.makeText(PostsActivity.this, "Your post has been made.",
+                        Toast.LENGTH_LONG).show();
+            }
+        });
+
+        setValueTask.addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+                Toast.makeText(PostsActivity.this,
+                        "Sorry! We couldn't make your post!" + e.toString(),
+                        Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
 }
